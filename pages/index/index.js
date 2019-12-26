2f413f289581a23f1870ebef2e90d1b2abd52605
uborfly <zkx1006@126.com>
2019-12-26 13:56:16 +0800

    新增功能及优化
    1、加入过程动态显示
    2、优化解析逻辑

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,122 +15,56 @@ function randomNum(minNum, maxNum) {
       return 0;
       break;
   }
-} 
+}
 
+var timeout = 60;
+var out = null;
 Page({
-  
+  // this.setData({ msg: "Hello World" });
+
   data: {
-    getInput: null
+    getInput: null,
+    message: "结果显示"
   },
-    getInput: function (e) {//方法1
+
+  getInput: function(e) { //方法1
     this.data.getInput = e.detail.value;
   },
-  clickMe: function (e) {
-    //获取input
-    var inputed = this.data.getInput;
-    //解析数量
-    var i = 0;
-    var num = 0;
-    for(i in inputed)
-    {
-      if (inputed[i] == '\/')
-        num++;
-    }
-    console.log("total num",num);
-    //取随机数
-    var order = randomNum(1, num);
-    console.log("order",order);
-    //找第order个'/'
-    var outBuf;
-    i=0;
-    var j=0;
-    for (i in inputed) {
-      if (inputed[i] == '\/' )
-      {
-        order--;
-        console.log("i,j", i,j);
-        if(order == 0)
-        {
-          console.log("i-j", i-j);
-          outBuf = inputed.substring(i-j);
-          break;
-        } 
-        j = -1;
-      }
-      j++;
-    }
 
-    console.log("outBuf", outBuf);
-    var outPos,cnt= 0;
-    i=0;
-    for (i in outBuf) {
-      if (outBuf[i] == '\/') {
-          outPos = i;
-          break;
-      }
-    }
-    console.log("outPos", outPos);
-    var out = outBuf.substring(0, outPos);
-    console.log("out", out);
+  clickMe: function(e) {
+    //获取input
+    if (this.data.getInput == null)
+      return;
+    var inputed = this.data.getInput;
+    var item = inputed.split('\/');
     //显示结果
-    wx.showModal({
-      title: '提示',
-      content: out,
-      success(res) {
-        if (res.confirm) {
-          console.log('用户点击确定')
-        } else if (res.cancel) {
-          console.log('用户点击取消')
-        }
-      }
-    })
-  },
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+    var self = this;
+    var outInter = setInterval(function() {
+      var num = item.length - 1;
+
+      //取随机数
+      var order = randomNum(1, num);
+
+      out = item[order - 1];
+      self.setData({
+        message: out
       })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+      if (timeout-- == 0) {
+        console.log("end", out);
+        clearInterval(outInter);
+        timeout = 60;
+        wx.showModal({
+          title: '提示',
+          content: out,
+          success(res) {
+            if (res.confirm) {
+              console.log('用户点击确定')
+            } else if (res.cancel) {
+              console.log('用户点击取消')
+            }
+          }
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    }, 50);
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
